2a19992f3b01079e4efb90fe950ac49d274200d3
Hanzala-Alam <hanzalaalamjaan54@gmail.com>
2024-01-24 01:34:33 +0500

    routing work

--- WebApp/src/app/app-routing.module.ts
+++ WebApp/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Router, RouterModule, Routes } from '@angular/router';
 import {HomeComponent} from './home/home.component';
 import {PreviewComponent} from './preview/preview.component';
+import {MediaPlayerComponent} from './media-player/media-player.component';
 
 const routes: Routes = [
   {path:"", component: HomeComponent},
   {path:"Home", component: HomeComponent},
   {path:"Preview/:id", component: PreviewComponent},
+  {path:"MediaPlayer/:id?", component: MediaPlayerComponent},
 ];
 
 @NgModule({
--- WebApp/src/app/media-player/media-player.component.ts
+++ WebApp/src/app/media-player/media-player.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {ThemesService} from '../Theme/themes.service';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-media-player',
   templateUrl: './media-player.component.html',
   styleUrls: ['./media-player.component.scss']
 })
-export class MediaPlayerComponent {
+export class MediaPlayerComponent implements OnInit {
+  _themes: ThemesService
 
+  constructor(private themes:ThemesService, private route:ActivatedRoute){
+    this._themes = themes;
+  }
+
+  ngOnInit(): void {
+    
+  }
 }
